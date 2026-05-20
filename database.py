c3ae2a59cb4b18a60671345d345a4f354a937868
MeixedoGabriel <meixedogabriel@gmail.com>
2026-05-20 20:14:34 -0300

    feat: impede concluir tarefas já concluídas

--- database.py
+++ database.py
@@ -12,7 +12,11 @@ def create_table():
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
-        status TEXT NOT NULL
+        status TEXT NOT NULL,
+        priority TEXT NOT NULL,
+        category TEXT NOT NULL,
+        created_at TEXT NOT NULL,
+        due_date TEXT NOT NULL
     )
     """)
 
